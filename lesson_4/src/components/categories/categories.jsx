3ae07b218ee14f17bbf290a72f6698e7f65cfe12
Dmitry <44106326+demid088@users.noreply.github.com>
2019-08-24 09:45:25 +0700

    Add files via upload

---
+++ lesson_4/src/components/categories/categories.jsx
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react';
+import ReactDom from 'react-dom';
+import './categories.css';
+
+export default class Search extends Component {
+  render() {
+    //const { title, type } = this.props; // props = object
+    return (
+      <Fragment>
+        <div class="card my-4">
+          <h5 class="card-header">Categories</h5>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">Web Design</a>
+                  </li>
+                  <li>
+                    <a href="#">HTML</a>
+                  </li>
+                  <li>
+                    <a href="#">Freebies</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">JavaScript</a>
+                  </li>
+                  <li>
+                    <a href="#">CSS</a>
+                  </li>
+                  <li>
+                    <a href="#">Tutorials</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
